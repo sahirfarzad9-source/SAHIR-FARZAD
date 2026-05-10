2a3707fd6f5d26e83f540fd37044db75119182f4
SAHIR <sahirfarzad9@gmail.com>
2026-05-10 21:20:36 +0600

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,20 @@
+name ="Sahir"
+age = 1
+is_student = True
+weight = 38.12
+
+print("Name:", name)
+print("Data type of name is", type(name))
+print("Age",age)
+print("Data type of age is", type(age))
+print("is_student", is_student)
+print("Data type of is_student is", type(is_student))
+print("Weight", weight);
+print("Data type of Weight is", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print("Data type of Weight is", type(weight));
